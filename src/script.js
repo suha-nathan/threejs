b94fed6615e67e8e93eae0c4c5b5b31d53006352
Suhasini Padmanathan <suhasini_95@hotmail.com>
2023-04-24 20:42:58 -0400

    changing 3D text

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Suhasini P',
             {
                 font: font,
                 size: 0.5,
