e4e66d57127042a441b149d44a94b632d7e64849
Suhasini Padmanathan <suhasini_95@hotmail.com>
2023-04-24 20:49:11 -0400

    changed text again

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Suhasini P',
+            'Suha Nathan',
             {
                 font: font,
                 size: 0.5,
